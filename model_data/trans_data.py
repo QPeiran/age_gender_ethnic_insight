c312478ea89820388569f882cb80154cda6f5b20
QPeiran <quanpeiran@gmail.com>
2020-03-31 23:11:34 +1300

    debug

--- model_data/trans_data.py
+++ model_data/trans_data.py
@@ -11,7 +11,7 @@ job_config = bigquery.QueryJobConfig(destination=table_id, write_disposition="WR
 #### 2.Due to the granularities of the "income_table" -- "Area" indicated by "Region" is what we want to focus
 #### 3.We want to analysis the labour market, so I assume the age/age group should be between "15 - 70 years old" (people too young or old cannot enter the labour market)
 query_value_data = """
-SELECT Area, Age, CAST(Count AS INT64) as Count_n FROM `countdownintervewtest.CDI_test.raw_main_table`
+SELECT Area, Age, CAST(Count AS INT64) as count FROM `countdownintervewtest.CDI_test.raw_main_table`
 WHERE (Area IN ("01", "02", "03", "04", "05", "06", "07", "08", "09", "12", "13", "14", "15", "16", "17", "18"))
     AND (Age IN ("015", "016", "017", "018", "019", "020", "021", "022", "023", "024", "025", "026", "027", "028", "029", "030", "031", "032", "033", "034", 
                  "035", "036", "037", "038", "039", "040", "041", "042", "043", "044", "045", "046", "047", "048", "049", "050", "051", "052", "053", "054",
@@ -19,6 +19,7 @@ WHERE (Area IN ("01", "02", "03", "04", "05", "06", "07", "08", "09", "12", "13"
     AND (Sex = "9") # total sex
     AND (Ethnic = "9999")   # total ethnic
     AND (Year = "2018") # year 2018
+    ORDER BY Age
 """
 
 query_job = bq_client.query(query_value_data, job_config=job_config)
